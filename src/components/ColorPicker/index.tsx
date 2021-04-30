70169e9a746222127792b1344138c6399fa74b0c
Amyr Ahmady <hhm6@yahoo.com>
2021-04-30 05:06:43 +0430

    create color picker component

---
+++ src/components/ColorPicker/index.tsx
@@ -0,0 +1,62 @@
+import React, { Component, useEffect, useState } from 'react';
+import { View, StyleSheet, StyleProp, ViewStyle, Image, Pressable, Linking, Switch, Text, ColorValue } from 'react-native';
+import { heightPercentageToDP as hp, widthPercentageToDP as wp } from '../../utils/screensize';
+import RoundCard from '../RoundCard';
+
+interface Props {
+    title: string;
+    titleColor: string;
+    eachRowCount: number;
+    onSelect?: (color: string) => void;
+    colors?: string[];
+    style?: ViewStyle
+}
+
+const ColorPicker = (props: Props) => {
+
+    const [parentWidth, setParentWidth] = useState(0);
+    const [colorItemSize, setColorItemSize] = useState(0);
+
+    const {
+        title,
+        titleColor,
+        onSelect,
+        eachRowCount,
+        colors,
+        style
+    } = props;
+
+    useEffect(() => {
+        setColorItemSize(
+            ((eachRowCount - 1) / (colors.length ? colors.length : 1)) * parentWidth
+        )
+    }, [parentWidth]);
+
+    console.log(colorItemSize, eachRowCount, colors.length);
+    return (
+        <RoundCard color="#ccc" padding={20} style={style} shadowed>
+            <Text style={{ fontSize: wp(1), color: titleColor, marginBottom: 10 }}>{title}</Text>
+            <View style={{ flexWrap: 'wrap', flexDirection: 'row', justifyContent: 'space-between', width: '100%' }} onLayout={(event) => setParentWidth(event.nativeEvent.layout.width)}>
+                {colors && colors.map((color, index) => {
+                    return (
+                        <Pressable
+                            style={{
+                                width: colorItemSize - 5, height: colorItemSize - 5, backgroundColor: color,
+                                marginBottom: 5, borderWidth: 0.5, borderColor: '#555'
+                            }}
+                            key={index}
+                            accessibilityRole="link"
+                            onPress={() => {
+                                if (onSelect)
+                                    onSelect(color);
+                            }}
+                        />
+                    )
+                })}
+            </View>
+        </RoundCard >
+    );
+
+}
+
+export default React.memo(ColorPicker);
