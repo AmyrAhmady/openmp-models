f8d89fd60bce3d46d13ee1d3eac6e8eee039022a
Amyr Ahmady <hhm6@yahoo.com>
2021-04-30 05:39:29 +0430

    a list of real names for keys of received data

---
+++ src/resources/realNames.tsx
@@ -0,0 +1,16 @@
+export default {
+  id: "Model ID",
+  model: "Model Name",
+  name: "Name",
+  cat: "Category",
+  mods: "Modification",
+  location: "Location",
+  gender: "Gender",
+  col: "Has Colision",
+  breakable: "Is Breakable",
+  visibility: "Visibility Status",
+  anime: "Is Animated",
+  radius: "Radius",
+  borderbox: "Border Box",
+  txd: "Texture File"
+}
