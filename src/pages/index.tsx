3581644864b8cc7d19735d214ace798f9fa8db3f
Amyr Ahmady <hhm6@yahoo.com>
2021-04-30 05:06:56 +0430

    add right sidebar and color picker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import ColorPicker from 'src/components/ColorPicker';
 import Header from 'src/components/Header'
 import MenuDesktop from 'src/components/Menu/Desktop';
 import MenuMobile from 'src/components/Menu/Mobile';
@@ -39,6 +40,7 @@ export default class Main extends React.Component<Props, any> {
         const {
             isMobileView
         } = this.props;
+
         return (
             <View style={styles.container}>
                 <Header
@@ -61,31 +63,29 @@ export default class Main extends React.Component<Props, any> {
                     </View>
                     {isMobileView ? null : (
                         <SideBar isMobile={isMobileView} style={{ width: '20%' }}>
-                            <Text>Hey</Text>
+                            <ColorPicker
+                                title="Background color"
+                                titleColor="#555"
+                                style={{ marginHorizontal: 20, marginTop: 15 }}
+                                colors={[
+                                    '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231',
+                                    '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
+                                    '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000',
+                                    '#ffd8b1', '#000075', '#808080', '#ffffff', '#000000'
+                                ]}
+                                eachRowCount={5}
+                            />
                         </SideBar>
                     )}
                 </View>
             </View >
-        )
+        );
     }
-
 }
 
 const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
         height: '100%'
-    },
-    link: {
-        color: 'blue',
-    },
-    textContainer: {
-        alignItems: 'center',
-        marginTop: 16,
-    },
-    text: {
-        alignItems: 'center',
-        fontSize: 24,
-        marginBottom: 24,
     },
 })
