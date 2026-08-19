e1872109fa95101cc8b6254059d3127710d1fd4b
iAmir <hhm6@yahoo.com>
2026-08-19 23:41:17 +0330

    add favicon and title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -68,6 +68,7 @@ export default class MyDocument extends Document {
         return (
             <Html style={{ height: '100%' }}>
                 <Head>
+                    <link rel="icon" href="/favicon.ico" />
                     <link rel="preconnect" href="https://rsms.me" />
                     <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
                 </Head>
